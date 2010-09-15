de157e41ecdc050ec94281e102b660dbac210c5a
Bruno Pedroso <Bruno@macasa.local>
2010-09-15 17:40:04 -0300

    passando dois testes no dojo (alone cell)

--- spec/unit/spec.js
+++ spec/unit/spec.js
@@ -1,8 +1,22 @@
 
-describe 'YourLib'
-  describe '.someMethod()'
-    it 'should do something'
-      true.should.be true
-    end
-  end
+describe 'GameOfLife'
+
+	describe 'alone cell'
+	
+	   it 'should live'
+		game = new GameOfLife();
+		game.isAlive(0,0).should.be false
+		game.setAlive(0,0);
+		game.isAlive(0,0).should.be true
+	   end
+	
+	   it 'should die in the next generation'
+		game = new GameOfLife();
+		game.setAlive(0,0);
+		game.next();
+		game.isAlive(0,0).should.be false
+	   end
+	
+	end
+
 end
