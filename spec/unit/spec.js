b163f630bb50540f7a32bcb90abd1da29e7dc490
Bruno Pedroso <Bruno@macasa.local>
2010-09-15 18:00:26 -0300

    refatoracao para inclusao de matriz. Todos os testes passando.

--- spec/unit/spec.js
+++ spec/unit/spec.js
@@ -2,21 +2,32 @@
 describe 'GameOfLife'
 
 	describe 'alone cell'
+		it 'should live when setAlive is used'
+			game = new GameOfLife();
+			game.isAlive(0,0).should.be false
+			game.setAlive(0,0);
+			game.isAlive(0,0).should.be true
+		end
+
+		it 'should die in the next generation'
+			game = new GameOfLife();
+			game.setAlive(0,0);
+			game.next();
+			game.isAlive(0,0).should.be false
+		end
+
+	end
 	
-	   it 'should live'
-		game = new GameOfLife();
-		game.isAlive(0,0).should.be false
-		game.setAlive(0,0);
-		game.isAlive(0,0).should.be true
-	   end
-	
-	   it 'should die in the next generation'
-		game = new GameOfLife();
-		game.setAlive(0,0);
-		game.next();
-		game.isAlive(0,0).should.be false
-	   end
-	
+	describe 'two neighbours'
+		it 'should live in the next generation'
+			game = new GameOfLife();
+			game.setAlive(0,0);
+			game.setAlive(1,0);
+			game.setAlive(0,1);
+			game.next();
+			game.isAlive(0,0).should.be true
+		end
+
 	end
 
 end
