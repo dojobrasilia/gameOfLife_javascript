816d65db7f0cf214bd933a88aa4c03f965fefa3c
Bruno A. Rolim <brolim@brolim-laptop.(none)>
2010-09-22 17:39:42 -0300

    ajustamos as libs do jspec para freeze

--- spec/rhino.js
+++ spec/rhino.js
@@ -1,7 +1,7 @@
 
-load('/Library/Ruby/Gems/1.8/gems/jspec-4.3.3/lib/jspec.js')
-load('/Library/Ruby/Gems/1.8/gems/jspec-4.3.3/lib/jspec.xhr.js')
-load('lib/yourlib.js')
+load('./spec/lib/jspec.js')
+load('./spec/lib/jspec.xhr.js')
+load('lib/gameOfLife.js')
 load('spec/unit/spec.helper.js')
 
 JSpec
