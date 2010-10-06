3d9d97fa5bc78978c9ef8dc19585386be8b83900
Bruno Pedroso <Bruno@MacAsa.local>
2010-10-06 17:59:24 -0300

    we have drawn rows and columns inside the table

--- spec/integration/visual_interface_spec.js
+++ spec/integration/visual_interface_spec.js
@@ -2,11 +2,32 @@ describe 'GameOfLife'
 
 	describe 'visual interface'
 		describe 'constructor'
-			it 'should draw the board in html'
+			before_each
 				div = $("#happy_board")
+				div.empty();
+			end
+		
+			it 'should draw the board in html'
 				game = new GameOfLife(3,3,div)
 				div.find("table").length.should.be 1
 			end
+
+				it 'should draw the right number of lines'
+					game = new GameOfLife(3,3,div)	
+					div.find("table").find("tr").length.should.be 3
+					div.empty()
+					game = new GameOfLife(4,3,div)	
+					div.find("table").find("tr").length.should.be 4
+				end
+
+			it 'should draw the right number of cols'
+				game = new GameOfLife(3,3,div)	
+				div.find("table").find("td").length.should.be 9
+				// div.empty()
+				// game = new GameOfLife(4,3,div)	
+				// div.find("table").find("tr").length.should.be 4
+			end
+			
 		end
 	end		
 
