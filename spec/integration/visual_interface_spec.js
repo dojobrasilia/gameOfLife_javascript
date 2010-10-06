b97154ea634b176ffc930b68dd5bf3c61360a6f9
Bruno Pedroso <Bruno@MacAsa.local>
2010-10-06 18:41:17 -0300

    we have set alive and set dead cells in the table

--- spec/integration/visual_interface_spec.js
+++ spec/integration/visual_interface_spec.js
@@ -22,10 +22,43 @@ describe 'GameOfLife'
 
 			it 'should draw the right number of cols'
 				game = new GameOfLife(3,3,div)	
-				div.find("table").find("td").length.should.be 9
-				// div.empty()
-				// game = new GameOfLife(4,3,div)	
-				// div.find("table").find("tr").length.should.be 4
+				div.find("table").find("td").length.should.be (3*3)
+				div.empty()
+				game = new GameOfLife(3,4,div)	
+				div.find("table").find("td").length.should.be (3*4)
+			
+			end
+			
+			it 'should add a live cell to the table'
+				game = new GameOfLife(3,3,div)
+				game.setAlive(1,1)
+				div.find("table").find("tr:nth-child(2)").find("td:nth-child(2)").text().should.be 'X'
+
+				game.setAlive(1,2)
+				div.find("table").find("tr:nth-child(2)").find("td:nth-child(3)").text().should.be 'X'
+			end
+			
+			it 'should remove a live cell from the table'
+				game = new GameOfLife(3,3,div)
+				game.setAlive(1,1)
+				game.setDead(1,1)
+				div.find("table").find("tr:nth-child(2)").find("td:nth-child(2)").html().should.be '&nbsp;'
+				game.setAlive(1,2)
+				game.setDead(1,2)
+				div.find("table").find("tr:nth-child(2)").find("td:nth-child(3)").html().should.be '&nbsp;'
+			end
+			
+			it 'should blink in the html'
+				game = new GameOfLife(3,3,div)
+				game.setAlive(1,0)
+				game.setAlive(1,1)
+				game.setAlive(1,2)
+				game.next();
+				div.find("table").find("tr:nth-child(1)").find("td:nth-child(2)").html().should.be 'X'
+				div.find("table").find("tr:nth-child(3)").find("td:nth-child(2)").html().should.be 'X'
+				div.find("table").find("tr:nth-child(2)").find("td:nth-child(1)").html().should.be '&nbsp;'
+				div.find("table").find("tr:nth-child(2)").find("td:nth-child(3)").html().should.be '&nbsp;'
+				
 			end
 			
 		end
